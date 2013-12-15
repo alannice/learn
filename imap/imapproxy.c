754a0d91d633f7adbf29b9937907183b836b942d
alannice <alannice@126.com>
2013-12-15 06:11:23 -0800

    modify imap

--- imap/imapproxy.c
+++ imap/imapproxy.c
@@ -21,23 +21,23 @@
 
 #define CLI_STRLEN 255
 #define BUFSIZE_MAX 8192
-#define LINE_MAX   1024
+#define LINE_MAX   998  // rfc...
 
 struct client_t {
-	char user[CLI_STRLEN];
-	char passwd[CLI_STRLEN];
+	char user[CLI_STRLEN+1];
+	char passwd[CLI_STRLEN+1];
 	struct xyz_buf_t *bufin;
 	struct xyz_buf_t *bufout;
-	char tag[CLI_STRLEN];
-	char cmd[CLI_STRLEN];
-	char args[CLI_STRLEN];
+	char tag[CLI_STRLEN+1];
+	char cmd[CLI_STRLEN+1];
+	char args[CLI_STRLEN+1];
 	int status;
 	int errcmd;
 	time_t lastcmd;             
 	time_t starttime;               
 
-	char cliaddr[CLI_STRLEN];
-	char servaddr[CLI_STRLEN];              
+	char cliaddr[CLI_STRLEN+1];
+	char servaddr[CLI_STRLEN+1];              
 	int servport;
 };  
 
@@ -77,7 +77,8 @@ int client_init()
 
 	g_client.lastcmd = g_client.starttime = time(NULL);
 
-	xyz_buf_add(g_client.bufout, "* OK IMAP Proxy ready\r\n", 23);
+	char *msg = "* OK IMAPrev1 Proxy ready\r\n";
+	xyz_buf_add(g_client.bufout, msg, strlen(msg));
 	xyz_buf_write(g_client.bufout, STDOUT_FILENO);
 
 	strcpy(g_client.cliaddr, "10.10.10.10");
@@ -88,16 +89,20 @@ int client_init()
 int client_check(void)
 {
 	if((time(NULL)-g_client.lastcmd) > g_setting.timeout) {
-		xyz_buf_add(g_client.bufout, "* OK IMAP Proxy timeout bye\r\n", 29);
-		xyz_buf_write(g_client.bufout, STDOUT_FILENO);
+		printf("client_check :: timeout closed\n");
 		xyz_event_stop(g_event);
 	}
 
 	if(g_client.errcmd > g_setting.errcmd) {
-		xyz_buf_add(g_client.bufout, "* OK IMAP command error to many bye\r\n", 29);                                     
-		xyz_buf_write(g_client.bufout, STDOUT_FILENO);                                                           
+		printf("client_check :: to many error\n");
 		xyz_event_stop(g_event);    
 	}
+
+	return 0;
+}
+
+int our_auth() 
+{
 
 	return 0;
 }
@@ -105,31 +110,59 @@ int client_check(void)
 
 int cmd_login(void)
 {
+	char *user, *passwd;
+	int flag = 0;
+
 	printf("login :: tag:[%s], cmd:[%s], args:[%s]\n", g_client.tag, g_client.cmd, g_client.args);
 
-	if(strlen(g_client.args) == 0) {
-		printf("login :: args length is 0\n");
-		return -1;
-	}
+	do {
+		if(strlen(g_client.args) == 0) {
+			printf("login :: args length is 0\n");
+			break;
+		}
 
-	char *user = g_client.args;
-	char *passwd = strchr(user, ' ');
-	if(passwd == NULL) {
-		printf("login :: args no space\n"); 
+		user = g_client.args;
+		passwd = strchr(user, ' ');
+		if(passwd == NULL) {
+			printf("login :: args no space\n"); 
+			break;
+		}
+		*passwd = '\0';
+		if(strlen(user) == 0) {
+			printf("login :: no user name\n");
+			break;
+		}
+		passwd++;
+		if(strlen(passwd) == 0) {
+			printf("login :: passwd length is 0\n"); 
+			break;
+		}
+		if(strchr(passwd, ' ') != NULL) {
+			printf("login :: passwd has space\n");
+			break;
+		}
+
+		flag = 0;
+	} while (0);
+
+	if(flag == 0) {
+		char *msg1 = "BAD Request\r\n";
+		
+		xyz_buf_sprintf(g_client.bufout, "%s %s", g_client.tag, msg1);
+		xyz_buf_write(g_client.bufout, STDOUT_FILENO); 
+
 		return -1;
-	}
-	*passwd = '\0';
-	if(strlen(user) == 0) {
-		printf("login :: no user name\n");
-		return -1;
-	}
-	passwd++;
-	if(strlen(passwd) == 0) {
-		printf("login :: passwd length is 0\n"); 
-		return -1;
-	}
-	if(strchr(passwd, ' ') != NULL) {
-		printf("login :: passwd has space\n");
+	} 
+
+	// auth ...
+	
+	int retval = our_auth();
+	if(retval == -1) {
+		char *msg1 = "NO LOGIN Login or password error\r\n";
+
+		xyz_buf_sprintf(g_client.bufout, "%s %s", g_client.tag, msg1);
+		xyz_buf_write(g_client.bufout, STDOUT_FILENO);
+
 		return -1;
 	}
 
@@ -153,7 +186,11 @@ int cmd_logout(void)
 		return -1;
 	}
 
-	xyz_buf_add(g_client.bufout, "* OK IMAP Proxy logout bye\r\n", 28);
+	char *msg1 = "* BYE IMAP4rev1 Proxy logging out\r\n";
+	char *msg2 = "OK LOGOUT completed\r\n";
+
+	xyz_buf_add(g_client.bufout, msg1, strlen(msg1));
+	xyz_buf_sprintf(g_client.bufout, "%s %s", g_client.tag, msg2);
 	xyz_buf_write(g_client.bufout, STDOUT_FILENO);
 	xyz_event_stop(g_event);
 
@@ -164,10 +201,19 @@ int cmd_capa(void)
 {
 	printf("capa :: tag:[%s], cmd:[%s], args:[%s]\n", g_client.tag, g_client.cmd, g_client.args);
 
+	/*
 	if(strlen(g_client.args) > 0) {
 		printf("capability :: has args error\n");
 		return -1;
 	}
+	*/
+
+	char *msg1 = "* CAPABILITY IMAP4rev1 ID STARTTLS UIDPLUS\r\n";
+	char *msg2 = "OK CAPABILITY completed\r\n";
+
+	xyz_buf_add(g_client.bufout, msg1, strlen(msg1));
+	xyz_buf_sprintf(g_client.bufout, "%s %s", g_client.tag, msg2);
+	xyz_buf_write(g_client.bufout, STDOUT_FILENO);
 
 	return 0;
 }
@@ -176,10 +222,17 @@ int cmd_noop(void)
 {
 	printf("noop :: tag:[%s], cmd:[%s], args:[%s]\n", g_client.tag, g_client.cmd, g_client.args);
 
+	/*
 	if(strlen(g_client.args) > 0) {
 		printf("noop :: has args error\n");
 		return -1;
 	}
+	*/
+
+	char *msg = "OK NOOP completed\r\n";
+
+	xyz_buf_sprintf(g_client.bufout, "%s %s", g_client.tag, msg);
+	xyz_buf_write(g_client.bufout, STDOUT_FILENO);
 
 	return 0;
 }
@@ -188,9 +241,37 @@ int cmd_auth(void)
 {
 	printf("auth :: tag:[%s], cmd:[%s], args:[%s]\n", g_client.tag, g_client.cmd, g_client.args);
 
+	/*
 	if(strlen(g_client.args) > 0) {
 		printf("auth :: has args error\n");
 		return -1;
+	}
+	*/
+
+	char *msg = "BAD command not support\r\n";
+
+	xyz_buf_sprintf(g_client.bufout, "%s %s", g_client.tag, msg);
+	xyz_buf_write(g_client.bufout, STDOUT_FILENO);
+
+	return 0;
+}
+
+int cmd_id(void)
+{
+	printf("id :: tag:[%s], cmd:[%s], args:[%s]\n", g_client.tag, g_client.cmd, g_client.args);
+
+	if(strlen(g_client.args) > 0) {
+		char *msg1 = "* ID (\"name\" \"sina imap server\" \"vendor\" \"sina\")\r\n";
+		char *msg2 = "OK ID Completed\r\n";
+
+		xyz_buf_add(g_client.bufout, msg1, strlen(msg1));
+		xyz_buf_sprintf(g_client.bufout, "%s %s", g_client.tag, msg2);
+		xyz_buf_write(g_client.bufout, STDOUT_FILENO);
+	} else {
+		char *msg = "BAD Invalid argument\r\n";
+
+		xyz_buf_sprintf(g_client.bufout, "%s %s", g_client.tag, msg);
+		xyz_buf_write(g_client.bufout, STDOUT_FILENO);
 	}
 
 	return 0;
@@ -200,13 +281,18 @@ int cmd_nosupport(void)
 {
 	printf("nosupport :: tag:[%s], cmd:[%s], args:[%s]\n", g_client.tag, g_client.cmd, g_client.args);
 
-	return 0;
+	char *msg = "BAD Please login first\r\n";
+
+	xyz_buf_sprintf(g_client.bufout, "%s %s", g_client.tag, msg);
+	xyz_buf_write(g_client.bufout, STDOUT_FILENO);
+
+	return -1;
 }
 
 int client_read(int fd, void *arg)
 {
 	char *tag, *cmd, *args;
-	int n;
+	int n, flag;
 
 	n = xyz_buf_read(g_client.bufin, fd);
 	if(n == -1) {
@@ -224,6 +310,7 @@ int client_read(int fd, void *arg)
 		printf("not full line get\n");
 		return 0;
 	} else if(n == -2) {
+		printf("line too lang drop it\n");
 		int len = strchr(xyz_buf_data(g_client.bufin), '\n') - xyz_buf_data(g_client.bufin);
 		xyz_buf_drop(g_client.bufin, len+1);
 		return 0;
@@ -237,52 +324,66 @@ int client_read(int fd, void *arg)
 	bzero(g_client.tag, sizeof(g_client.tag));
 	bzero(g_client.args, sizeof(g_client.args));
 
-	// get tag.
-	tag = g_line;
-	cmd = strchr(tag, ' ');
-	if(cmd == NULL) {
-		// error
-		printf("line no space\n");
-		g_client.errcmd++;
-		return 0;
-	}
-	*cmd = '\0';
-	if(strlen(tag) == 0) {
-		printf("tag length is 0\n");
-		g_client.errcmd++;
-		return 0;
-	}
-	strncpy(g_client.tag, tag, sizeof(g_client.tag)-1);
+	flag = 0;
+	do {
+		// get tag.
+		tag = g_line;
+		cmd = strchr(tag, ' ');
+		if(cmd == NULL) {
+			// error
+			printf("line no space\n");
+			strncpy(g_client.tag, tag, sizeof(g_client.tag)-1);
+			break;
+		}
+		*cmd = '\0';
+		if(strlen(tag) == 0) {
+			printf("tag length is 0\n");
+			strncpy(g_client.tag, "*", 1);
+			break;
+		}
+		strncpy(g_client.tag, tag, sizeof(g_client.tag)-1);
 
-	// get cmd
-	cmd++;
-	if(strlen(cmd) == 0) {
-		// error
-		printf("command length is 0\n");
-		g_client.errcmd++;
-		return 0;
-	}
-	// get args
-	args = strchr(cmd, ' ');
-	if(args == NULL) {
-		strncpy(g_client.cmd, cmd, sizeof(g_client.cmd)-1);
-	} else {
-		*args = '\0';
+		// get cmd
+		cmd++;
 		if(strlen(cmd) == 0) {
+			// error
 			printf("command length is 0\n");
-			g_client.errcmd++;
-			return 0;
+			break;
 		}
-		strncpy(g_client.cmd, cmd, sizeof(g_client.cmd)-1);
-
-		// get arg
-		args++;	
-		if(strlen(args) > 0) {
-			strncpy(g_client.args, args, sizeof(g_client.args)-1);
+		// get args
+		args = strchr(cmd, ' ');
+		if(args == NULL) {
+			strncpy(g_client.cmd, cmd, sizeof(g_client.cmd)-1);
 		} else {
-			printf("the end is a space\n");
-			return 0;
+			*args = '\0';
+			if(strlen(cmd) == 0) {
+				printf("command length is 0\n");
+				break;
+			}
+			strncpy(g_client.cmd, cmd, sizeof(g_client.cmd)-1);
+
+			// get arg
+			args++;	
+			if(strlen(args) > 0) {
+				strncpy(g_client.args, args, sizeof(g_client.args)-1);
+			} else {
+				printf("the end is a space\n");
+				break;
+			}
 		}
+
+
+		flag = 1;
+	} while(0);
+
+
+	if(flag == 0) {
+		char *msg = "BAD command not support\r\n";
+
+		xyz_buf_sprintf(g_client.bufout, "%s %s", g_client.tag, msg);
+		xyz_buf_write(g_client.bufout, STDOUT_FILENO);
+		
+		return 0;
 	}
 
 
@@ -294,6 +395,8 @@ int client_read(int fd, void *arg)
 		n = cmd_capa();
 	} else if(strcasecmp(g_client.cmd, "authenticate") == 0) {
 		n = cmd_auth();
+	} else if(strcasecmp(g_client.cmd, "id") == 0) {
+		n = cmd_id();
 	} else if(strcasecmp(g_client.cmd, "noop") == 0) {
 		n = cmd_noop();
 	} else {
