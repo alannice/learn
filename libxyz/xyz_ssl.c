81551da35b2f2a568f4181e5e3260d86b45fe0d7
alannice <alannice@126.com>
2013-12-17 02:23:08 -0800

    modify lib

---
+++ libxyz/xyz_ssl.c
@@ -0,0 +1,119 @@
+
+#include "xyz_ssl.h"
+
+struct xyz_ssl_t *xyz_ssl_create(int method, char *pemfile)
+{
+    if((method != XYZ_SSLv23 && method != XYZ_TLSv1) &&  pemfile == NULL) {
+        return NULL;
+    }
+
+    struct xyz_ssl_t *ossl = malloc(sizeof(struct xyz_ssl_t));
+    if(ossl == NULL) {
+        return NULL;
+    }
+
+    ossl->method = method;
+
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+
+    if(method == XYZ_SSLv23) {
+        ossl->ctx = SSL_CTX_new(SSLv23_server_method());
+    } else if(method == XYZ_TLSv1) {
+        ossl->ctx = SSL_CTX_new(TLSv1_server_method());
+    } else {
+        free(ossl);
+        return NULL;
+    }
+
+    if (ossl->ctx == NULL) {
+        ERR_print_errors_fp(stdout);
+        SSL_CTX_free(ossl->ctx);
+        free(ossl);
+        return NULL;
+    }
+
+    if(SSL_CTX_use_certificate_file(ossl->ctx, pemfile, SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stdout);
+        SSL_CTX_free(ossl->ctx);
+        free(ossl);
+        return NULL;
+    }
+
+    if(SSL_CTX_use_PrivateKey_file(ossl->ctx, pemfile, SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stdout);
+        SSL_CTX_free(ossl->ctx);
+        free(ossl);
+        return NULL;
+    }
+
+    if (!SSL_CTX_check_private_key(ossl->ctx)) {
+        ERR_print_errors_fp(stdout);
+        SSL_CTX_free(ossl->ctx);
+        free(ossl);
+        return NULL;
+    }
+    
+    return ossl;
+}
+
+int xyz_ssl_accept(struct xyz_ssl_t *ossl, int sockfd)
+{
+    if(ossl == NULL || sockfd < 0) {
+        return -1;
+    }
+
+    ossl->ssl = SSL_new(ossl->ctx);
+    if(ossl->ssl == NULL) {
+        return -1;
+    }
+
+    ossl->sockfd = sockfd;
+
+    SSL_set_fd(ossl->ssl, sockfd);
+    if (SSL_accept(ossl->ssl) == -1) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int xyz_ssl_connect(struct xyz_ssl_t *ossl, int sockfd)
+{
+    if(ossl == NULL || sockfd < 0) {
+        return -1;
+    }
+
+    ossl->ssl = SSL_new(ossl->ctx);
+    if(ossl->ssl == NULL) {
+        return -1;
+    }
+
+    SSL_set_fd(ossl->ssl, sockfd);
+    if (SSL_connect(ossl->ssl) == -1) {
+        return -1;
+    }
+
+    return 0;
+}
+
+void xyz_ssl_destroy(struct xyz_ssl_t *ossl)
+{
+    if(ossl == NULL) {
+        return;
+    }
+    
+    if(ossl->ssl) {
+        SSL_shutdown(ossl->ssl);
+        SSL_free(ossl->ssl);
+    }
+    if(ossl->ctx) {
+        SSL_CTX_free(ossl->ctx);
+    }
+
+    free(ossl);
+
+    return;
+}
+
---
+++ libxyz/xyz_ssl.h
@@ -0,0 +1,23 @@
+
+#ifndef __XYZ_SSL_H__
+#define __XYZ_SSL_H__
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#define XYZ_SSLv23  1
+#define XYZ_TLSv1   2
+
+struct xyz_ssl_t {
+    SSL_CTX *ctx;    
+    SSL *ssl;
+    int method;
+    int sockfd;
+};
+
+struct xyz_ssl_t *xyz_ssl_create(int method, char *pemfile); 
+int xyz_ssl_accept(struct xyz_ssl_t *ssl, int sockfd);  
+int xyz_ssl_connect(struct xyz_ssl_t *ssl, int sockfd);  
+void xyz_ssl_destroy(struct xyz_ssl_t *ssl);  
+
+#endif // __XYZ_SSL_H__
