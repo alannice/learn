512a148de6cdf5ac9777951566268ca7c4690d19
alannice <alannice@126.com>
2014-01-08 23:43:18 -0800

    add some file

---
+++ imapd/imapd/imapd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "xyz_event.h"
+
+struct xyz_event_t *g_event;
+
+
+int main(int argc, char **argv)
+{
+    printf("hello world\n");
+
+    return 0;
+}
+
